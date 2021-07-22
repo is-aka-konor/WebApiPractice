72f7d32b0207af78aa9ba3ac850436ccf594027d
Sergey Idelson <aka.konor@gmail.com>
2021-07-22 21:37:46 +1200

    Added object mapper and tests

---
+++ WebApiPractice.Api/Mapper/ObjectMapper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiPractice.Api.Resources.Customer;
+using WebApiPractice.Persistent.DataModels;
+
+namespace WebApiPractice.Api.Mapper
+{
+    /// <summary>
+    /// Mapper between database objects and request/response objects
+    /// </summary>
+    public class ObjectMapper : IObjectMapper
+    {
+        #region Generic methods
+        public TDest Map<TSource, TDest>(TSource item)
+        {
+            object result = item switch
+            {
+                PostCustomerRequest postCustomerRequest => Map(postCustomerRequest),
+                Customer customer => Map(customer),
+                PostContactDetailsRequest postContactDetailsRequest => Map(postContactDetailsRequest),
+                ContactDetails contactDetails => Map(contactDetails),
+                _ => throw new NotSupportedException()
+            };
+
+            return (TDest)result;
+        }
+
+        public IEnumerable<TDest> Map<TSource, TDest>(IEnumerable<TSource> sourceCollection) => sourceCollection.Select(Map<TSource, TDest>);
+        #endregion
+
+        #region Private methods
+        private Customer Map(PostCustomerRequest request)
+        {
+            return new Customer()
+            {
+                CustomerExternalId = Guid.NewGuid(),
+                CreatedAt = DateTime.Now,
+                Status = request.Status.Value,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                ContactDetails = Map<PostContactDetailsRequest, ContactDetails>(request.ContactDetails).ToList()
+            };
+        }
+
+        private PostCustomerResponse Map(Customer customer)
+        {
+            return new PostCustomerResponse()
+            {
+                Status = customer.Status,
+                CreatedAt = customer.CreatedAt,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                ContactDetails = Map<ContactDetails, PostContactDetailsResponse>(customer.ContactDetails).ToList(),
+                CustomerExternalId = customer.CustomerExternalId
+            };
+        }
+
+        private ContactDetails Map(PostContactDetailsRequest request)
+        {
+            return new ContactDetails()
+            {
+                ContactDetailsType = request.ContactDetailsType.Value,
+                Details = request.Details
+            };
+        }
+
+        private PostContactDetailsResponse Map(ContactDetails contactDetails)
+        {
+            return new PostContactDetailsResponse()
+            {
+                ContactDetailsType = contactDetails.ContactDetailsType,
+                Details = contactDetails.Details
+            };
+        }
+        #endregion
+    }
+}
